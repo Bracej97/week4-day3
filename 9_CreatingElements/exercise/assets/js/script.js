0bc0933081d710fd1f29206e8ba50c225f0cc3d4
Joe Bracey <joebracey1@gmail.com>
2024-10-12 12:30:28 +0100

    Third exercise complete

--- 9_CreatingElements/exercise/assets/js/script.js
+++ 9_CreatingElements/exercise/assets/js/script.js
@@ -1,3 +1,18 @@
 // TODO: Add 2 new items to the sidebar called "Register" and "Help".
+var sideBarListEl = document.querySelector(".sidebar ul");
+
+var registerEl = document.createElement("li");
+registerEl.textContent = "Register";
+
+sideBarListEl.appendChild(registerEl);
 
 // TODO: MEGA CHALLENGE: can you add the Help link between Reports and Settings?
+
+var helpEl = document.createElement("li");
+helpEl.textContent = "Help";
+
+console.log(sideBarListEl.childNodes);
+
+sideBarListEl.insertBefore(helpEl, sideBarListEl.childNodes[6])
+
+//sideBarListEl.childNodes[7].insertBefore(helpEl);
